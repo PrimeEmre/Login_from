2cfe8a724b4e38fb8f5bbdc2694fbee41b454ca4
PrimeEmre <ynsemrgzl@gmail.com>
2025-01-17 13:30:35 -0500

    added the if and sles stament and I also aded the eorr mesage

--- login.js
+++ login.js
@@ -1,3 +1,19 @@
-function submit(){
-    
+function submit() {
+    // Reciving the values form html file 
+    let FirstName = document.getElementById("FirstName").value;
+    let LastName = document.getElementById("Last_Name").value;
+    let Email = document.getElementById("gmail").value;
+    let password = document.getElementById("password").value;
+
+    // Setting the  elements for if statment 
+    document.getElementById('FirstName').innerHTML = FirstName;
+    document.getElementById('Last_Name').innerHTML = LastName;
+    document.getElementById('gmail').innerHTML = Email;
+    document.getElementById('password').innerHTML = password;
+    // Added if and else statamet
+    if (FirstName && LastName && Email && password) {
+        alert("Thank you for filling the form");
+    } else {
+        alert("Please fill all the fields");
+    }
 }
